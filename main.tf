3c3cfb42bca7c1f7d21f4973cce4dd4cf8b863db
oks-spartakus <oks.spartakus@gmail.com>
2023-02-21 12:07:51 +0100

    configurable server with variables and output

--- main.tf
+++ main.tf
@@ -3,10 +3,41 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-    ami = "ami-0c55b159cbfafe1f0"
-    instance_type = "t2.micro"
+  ami                    = "ami-0c55b159cbfafe1f0"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
-    tags = {
-      Name = "terraform-example"
-    }
+  user_data = <<-EOF
+                #!/bin/bash
+                echo "Elo mordeczki!" > index.html
+                nohup busybox httpd -f -p ${var.server_port} &
+                EOF
+
+  tags = {
+    Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+variable "server_port" {
+  description = "Port number used by server for http requests"
+  type        = number
+  default     = 8080
+
+}
+
+output "public_ip" {
+  value       = aws_instance.example.public_ip
+  description = "Public IP address of www server"
+
 }
