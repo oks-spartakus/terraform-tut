00d7b410c5522ab18f5ae9f119987a61a002b9a9
oks-spartakus <oks.spartakus@gmail.com>
2023-02-23 10:28:01 +0100

    added aws backend for tf state

---
+++ state/outputs.tf
@@ -0,0 +1,10 @@
+output "s3_bucket_arn" {
+  value       = aws_dynamodb_table.terraform-locks.arn
+  description = "S3 bucket for tf state ARN"
+}
+
+output "dynamodb_table_name" {
+  value       = aws_dynamodb_table.terraform-locks.name
+  description = "DynamoDB table for tf state locks name"
+
+}
